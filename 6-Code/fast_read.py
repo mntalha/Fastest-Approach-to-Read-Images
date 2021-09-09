bdb026928bb11c7071eb2d312361c9000f4e3a76
mntalha <mntalha.kilic@hotmail.com>
2021-09-09 22:03:28 +0300

    Added Medium Turkish Doc
    Signed-off-by: mntalha <mntalha.kilic@hotmail.com>

--- 6-Code/fast_read.py
+++ 6-Code/fast_read.py
@@ -5,22 +5,22 @@ Created on Wed Sep  8 21:48:53 2021
 @author: talha
 """
 
-import os 
+import os
 
 
-#define img_path 
+#define img_path
 img_path = "../3-Data/train_img"
 
 
 #check path control
-if isinstance(img_path, str): #Image path is a string 
+if isinstance(img_path, str): #Image path is a string
     if os.path.exists(img_path):
         print ("Path is exist")
     else:
-        raise FileNotFoundError('Image is not found on your system')    
-    
+        raise FileNotFoundError('Path is not found on your system')    
 
-#measure elapsed time     
+
+#measure elapsed time
 import timeit
 
 
@@ -32,16 +32,16 @@ img_array = []
 #MATPLOTLIB READ
 import matplotlib.image as mpimg
 starttime = timeit.default_timer()
-for img in os.listdir(img_path):    
-    img_array.append(mpimg.imread(os.path.join(img_path, img))) 
+for img in os.listdir(img_path):
+    img_array.append(mpimg.imread(os.path.join(img_path, img)))
 print("The time difference is :", timeit.default_timer() - starttime)
 #The time difference is : 2.2727211
 
-# CV2 IMREAD  
+# CV2 IMREAD
 import cv2
 starttime = timeit.default_timer()
-for img in imgs_name:    
-    img_array.append(cv2.imread(os.path.join(img_path, img)))   
+for img in imgs_name:
+    img_array.append(cv2.imread(os.path.join(img_path, img)))
 print("The time difference is :", timeit.default_timer() - starttime)
 #The time difference is : 0.9522216000000001
 
@@ -50,18 +50,15 @@ print("The time difference is :", timeit.default_timer() - starttime)
 from PIL import Image
 import numpy as np
 starttime = timeit.default_timer()
-for img in imgs_name:    
-    img_array.append(np.asarray(Image.open(os.path.join(img_path, img))))   
+for img in imgs_name:
+    img_array.append(np.asarray(Image.open(os.path.join(img_path, img))))
 print("The time difference is :", timeit.default_timer() - starttime)
 #The time difference is : 2.0121553
 
 # IO IMREAD  FROM SKIMAGE
 import skimage.io as io
 starttime = timeit.default_timer()
-for img in os.listdir(img_path):    
-    img_array.append(io.imread(os.path.join(img_path, img))) 
+for img in os.listdir(img_path):
+    img_array.append(io.imread(os.path.join(img_path, img)))
 print("The time difference is :", timeit.default_timer() - starttime)
 # The time difference is : 3.799766
-
-
-
